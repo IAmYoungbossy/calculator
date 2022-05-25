e561139cbd2d1dc6aa0093578f30b740853cc92a
Letam Bossman Barinua <letambarinua@gmail.com>
2022-05-25 14:17:26 +0100

    Add arithmetic operator functions

--- main.js
+++ main.js
@@ -10,3 +10,24 @@ let storedOperator = [];
 let result;
 let backspace;
 let toString;
+
+/*Operator Functions*/
+function add(a,b) {
+    return a+b;
+}
+function subtract(a,b) {
+    return a-b;
+}
+function multiply(a,b) {
+    return a*b;
+}
+function divide(a,b) {
+    return a/b;
+}
+function findFactorial(factorial) {
+    let multiply = 1;
+    for (; factorial !== 0; factorial--) {
+        multiply *= factorial;
+    }
+    return multiply;
+}
