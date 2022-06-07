22b9b08cb07d8b6380dbbae73bb967e1b32e0087
Letam Bossman Barinua <letambarinua@gmail.com>
2022-06-07 12:48:00 +0100

    Add setTimeOut function to divide function

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const screen2 = document.querySelector('#screen2');
 const buttonContainer = document.querySelector('.buttoncontainer');
 const numberButtons = buttonContainer.querySelectorAll('.btn');
 const clearButton = buttonContainer.querySelector('#clear');
-const topScreenHistoryDisplay = document.querySelector('#calculator-display');
+const HistoryDisplay = document.querySelector('#calculator-display');
 const powerButton = document.querySelector('.fa');
 let storedValue = [], storedOperator = [], screenLength = [],
 clearScreen = [], backspace = [], screenDisplay;
@@ -21,7 +21,7 @@ const power = (a, b) => math(a, b, Math.round((a**b)*1000)/1000);
 function divide(a, b) {
 	if (b === 0) {
 		storedValue.push('Error'), screen.textContent = 'Error';
-		return;
+		setTimeout(() => clearAllNumbers(), 500);
 	} else return math(a, b, Math.round((a/b)*1000)/1000);
 }
 
@@ -52,9 +52,9 @@ function checkOperatorAndError(target) {
 		if (backspace.length < 1) return;
 		else {
 			clearScreen.push('=');
-			backspace = [...topScreenHistoryDisplay.textContent];
+			backspace = [...HistoryDisplay.textContent];
 			checkOperatorAndAssign();
-			topScreenHistoryDisplay.textContent = backspace.toString().replace(/,/g, '');
+			HistoryDisplay.textContent = backspace.toString().replace(/,/g, '');
 			backspace.push(+screen.textContent);
 			screenLength = [];
 		}
@@ -62,7 +62,7 @@ function checkOperatorAndError(target) {
 }
 
 function checkForError(symbol) {
-	if(clearScreen[clearScreen.length-1] == '=') topScreenHistoryDisplay.textContent = screen.textContent;
+	if(clearScreen[clearScreen.length-1] == '=') HistoryDisplay.textContent = screen.textContent;
 	if (screen.textContent == 'Error') {
 		storedValue = [], backspace = [];
 		storedValue.push('clear');
@@ -74,7 +74,7 @@ function checkForError(symbol) {
 		checkOperatorAndAssign();
 		storedValue = [];
 		pushOperatorToArray(symbol);
-		topScreenHistoryDisplay.textContent += symbol;
+		HistoryDisplay.textContent += symbol;
 	}
 }
 
@@ -93,7 +93,7 @@ function assignMathFunction(operate) {
         screen2.textContent = '', screen.textContent = 'Error!';
     else {
         screen.textContent = operate((storedValue[storedValue.length-2]),(storedValue[storedValue.length-1]));
-        topScreenHistoryDisplay.textContent = screen.textContent;
+        HistoryDisplay.textContent = screen.textContent;
         storedValue.push(parseFloat(screen.textContent));
         storedValue.push('clear');
         storedOperator = [];
@@ -111,7 +111,7 @@ function numberBtn(target,number){
     if (target === number && typeof storedValue[storedValue.length-1] === 'string')
     clearScreenForNewCalculation(target), backspace.push(parseInt(number));
     else if (target === number) {
-    screen.textContent += target, topScreenHistoryDisplay.textContent += target;
+    screen.textContent += target, HistoryDisplay.textContent += target;
     screenLength.push(number), backspace.push(parseInt(number));
     countScreenLength();
     }
@@ -129,16 +129,16 @@ function setTimeoutFunction(errorMsg){
 	setTimeout(function() {
 		let display = screenLength.join().replace(/,/g, '');
 		screen.textContent = display;
-		topScreenHistoryDisplay.textContent = display;
+		HistoryDisplay.textContent = display;
 	}, 500);
 }
 
 function clearScreenForNewCalculation(target) {
-    if (clearScreen[clearScreen.length-1] === '=') topScreenHistoryDisplay.textContent = '';
+    if (clearScreen[clearScreen.length-1] === '=') HistoryDisplay.textContent = '';
     clearScreen.pop();
     screen.textContent = '';
     storedValue.pop();
-    screen.textContent += target, topScreenHistoryDisplay.textContent += target;
+    screen.textContent += target, HistoryDisplay.textContent += target;
     screenLength.push(screen.textContent);
 	countScreenLength();
 }
@@ -146,25 +146,25 @@ function clearScreenForNewCalculation(target) {
 function addDecimal(target) {
 	if (target === '.' && typeof storedValue[storedValue.length-1] === 'string') {
 		if (storedValue[storedValue.length-1] === 'Error') {
-			topScreenHistoryDisplay.textContent = '';
-			screen.textContent = '0', topScreenHistoryDisplay.textContent = '0';
-			screen.textContent += target, topScreenHistoryDisplay.textContent += target;
+			HistoryDisplay.textContent = '';
+			screen.textContent = '0', HistoryDisplay.textContent = '0';
+			screen.textContent += target, HistoryDisplay.textContent += target;
 			storedValue = [];
 			screenLength.push('0'), screenLength.push('.');
 		} else {
-			topScreenHistoryDisplay.textContent = '0', screen.textContent = '0';
+			HistoryDisplay.textContent = '0', screen.textContent = '0';
 			storedValue.pop();
-			screen.textContent += target, topScreenHistoryDisplay.textContent += target;
+			screen.textContent += target, HistoryDisplay.textContent += target;
 			screenLength.push('0'), screenLength.push('.');
 		}
 	} else if (target === '.' && (screen.textContent).indexOf('.') >= 0) return;
 	else {
 		if (target === '.' && screen.textContent !== '') {
-			screen.textContent += target, topScreenHistoryDisplay.textContent += target;
+			screen.textContent += target, HistoryDisplay.textContent += target;
 			screenLength.push('.');
 		} else if (target === '.' && screen.textContent === '') {
-			screen.textContent = '0', topScreenHistoryDisplay.textContent = '0';
-			screen.textContent += target, topScreenHistoryDisplay.textContent += target;
+			screen.textContent = '0', HistoryDisplay.textContent = '0';
+			screen.textContent += target, HistoryDisplay.textContent += target;
 			screenLength.push('0'), screenLength.push('.');
 		}
 	}
@@ -179,16 +179,16 @@ function deleteRecentNumber(target) {
 		screenLength.pop();
         if (screenLength.length < 1 ) screen2.textContent = '0';
 		backspace = [], screenDisplay = [];
-		backspace = [...screen.textContent], screenDisplay = [...topScreenHistoryDisplay.textContent];
+		backspace = [...screen.textContent], screenDisplay = [...HistoryDisplay.textContent];
 		backspace.pop(), screenDisplay.pop();
-		screen.textContent = topScreenHistoryDisplay.textContent = '';
+		screen.textContent = HistoryDisplay.textContent = '';
 		screen.textContent = backspace.toString().replace(/,/g, '');
-		topScreenHistoryDisplay.textContent = screenDisplay.toString().replace(/,/g, '');
+		HistoryDisplay.textContent = screenDisplay.toString().replace(/,/g, '');
     }
 }
 
 function clearAllNumbers() {
-    screen.textContent = topScreenHistoryDisplay.textContent = '';
+    screen.textContent = HistoryDisplay.textContent = '';
     storedValue = [], storedOperator = [], screenLength = [], backspace = [];
     if (screenLength.length < 1) screen2.textContent = '0';
 }
